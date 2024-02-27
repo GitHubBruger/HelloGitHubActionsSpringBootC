f68de28c6f94910d7a9037bd43e0f188c3f46675
GitHubBruger <ianb@kea.dk>
2024-02-27 10:31:23 +0100

    Added controller

--- src/main/java/kea/iabr/hellogithubactionsspringbootc/HelloGitHubActionsSpringBootCApplication.java
+++ src/main/java/kea/iabr/hellogithubactionsspringbootc/HelloGitHubActionsSpringBootCApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HelloGitHubActionsSpringBootCApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HelloGitHubActionsSpringBootCApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(HelloGitHubActionsSpringBootCApplication.class, args);
+    }
 
 }
